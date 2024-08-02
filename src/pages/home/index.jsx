eecb6788319e1e3262b715f72c846fbfdd382279
gabrielssouza <gabriel.desouzasilva2014@gmail.com>
2024-08-02 19:01:18 -0300

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar />
+        <section className='bg-gradient-to-b from-green-400 via-green-300 to-gray-50 h-screen w-screen'>
+
+        </section>
+    </div>
+  )
+}
