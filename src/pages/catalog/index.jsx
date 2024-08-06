046c9dfeaf0ddbbf1124406730a6685830613655
gabrielssouza <gabriel.desouzasilva2014@gmail.com>
2024-08-06 18:55:34 -0300

    catalog page

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
+import OverflowCard from './overflowCard'
 
 export default function Catalog() {
     return (
         <div className="bg-custom w-screen justify-center flex">
-            <div className='m-12 w-full h-screen bg-zinc-50 overflow-hidden space-y-16 font-poppins'>
-                <Navbar currentPage='catalog'/>
+            <div className='m-12 w-full h-screen bg-zinc-50 overflow-hidden space-y-8 font-poppins'>
+                <Navbar currentPage='catalog' />
+                <h1 className='items-center justify-center flex text-4xl text-custom-green'>
+                    Catálogo de Espécies
+                </h1>
+                <div className="w-1/3 h-px bg-custom-green m-auto" />
+                <section className='flex gap-4 flex-wrap justify-center'>
+                    <OverflowCard />
+                    <OverflowCard />
+                    <OverflowCard />
+                    <OverflowCard />
+                    <OverflowCard />
+                    <OverflowCard />
+                    <OverflowCard />
+                </section>
             </div>
         </div>
     )
