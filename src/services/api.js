eb04d088b98f32b40acbc547d50e4a543cb0da70
Gabriel Souza <gabriel.desouzasilva2014@gmail.com>
2024-09-29 16:14:40 -0300

    admin implementations

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://carboapi.onrender.com",
+  baseURL: "https://carboapi.onrender.com",
 
-  baseURL: "http://localhost:3333",
+  // baseURL: "http://localhost:3333",
 });
 
 export default api;
